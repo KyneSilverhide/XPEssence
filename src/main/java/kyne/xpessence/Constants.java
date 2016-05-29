fd5847f7cd1e27e469d9e2596329bff6a44991e6
Aurélien Lansmanne <aurelien.lansmanne@gmail.com>
2016-05-29 18:06:26 +0200

    Update to 0.4 beta

--- src/main/java/kyne/xpessence/Constants.java
+++ src/main/java/kyne/xpessence/Constants.java
@@ -2,6 +2,6 @@ package kyne.xpessence;
 
 public class Constants {
     public static final String MODID = "xpessence";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.4";
     public static final String MOD_NAME = "XP Essence";
 }
